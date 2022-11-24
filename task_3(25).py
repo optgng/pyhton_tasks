ce0ebc8ccfcd5e4635aab3b460451c8ba0fd6d4b
Daniil D <dandov9@gmail.com>
2022-11-25 02:40:20 +0300

    added check for negative numbers

--- task_3(25).py
+++ task_3(25).py
@@ -1,11 +1,18 @@
 def rever_number(num):
     rev_num = 0
-    while num != 0:
-        digit = num % 10
-        num = num // 10
-        rev_num = rev_num * 10
-        rev_num = rev_num + digit
-    print("Обратное число: ", rev_num, end='')
+    if num > 0:
+        while (num != 0):
+            digit = int(num % 10)
+            rev_num = rev_num * 10 + digit
+            num = int(num / 10)
+        print("Обратное число: ", rev_num, end='')
+    else:
+        num = abs(num)
+        while (num != 0):
+            digit = int(num % 10)
+            rev_num = rev_num * 10 + digit
+            num = int(num / 10)
+        print('Обратное число: -' + str(rev_num))
 
 if __name__ == "__main__" :
     number = int(input("Введите целое число: "))
